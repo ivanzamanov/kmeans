d2e6b5903f7dbef52e934e46bc2d6da662aec853
Ivan Zamanov <ivan.zamanov@asteasolutions.net>
2014-02-10 00:40:55 +0200

    extract-artcles.sh does tokenization and stemming

--- extract-articles.sh
+++ extract-articles.sh
@@ -1,8 +1,15 @@
 REUTERS_DIR=data/reuters21578/
 EXTRACT_DIR=/tmp/extracted
+STEM_DIR=/tmp/stemmed
+
+set -e
 
 if [ ! -d $EXTRACT_DIR ]; then
   mkdir -p $EXTRACT_DIR
+fi
+
+if [ ! -d $STEM_DIR ]; then
+  mkdir -p $STEM_DIR
 fi
 
 ARTICLE_COUNT=0
@@ -12,5 +19,17 @@ do
   ARTICLE_COUNT=$(($ARTICLE_COUNT+${EXTRACT_OUTPUT##*:}))
   echo "Articles: $ARTICLE_COUNT"
 done
-
 echo "Total articles: $ARTICLE_COUNT"
+
+echo "Tokenizing and stemming"
+COUNT=0
+for file in `ls $EXTRACT_DIR`
+do
+	./bin/tokenizer < $EXTRACT_DIR/$file | ./bin/stemmer > $STEM_DIR/$file
+#	./bin/tokenizer < $EXTRACT_DIR/$file > $STEM_DIR/$file
+	COUNT=$(($COUNT + 1))
+	if [ $(($COUNT % 100)) == 0 ]; then
+		echo "$COUNT"
+	fi
+done
+
