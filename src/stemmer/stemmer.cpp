320bd11f278732277145bc9e677baa810f6a7d4a
Ivan Zamanov <ivan.zamanov@asteasolutions.net>
2014-02-09 13:07:54 +0200

    Add stemmer skeleton

---
+++ src/stemmer/stemmer.cpp
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+#include"utils.h"
+
+void processFile(int inputFD, int outputFD, struct stat* fileStat);
+
+int main(int argc, char** argv) {
+	int inputFD = -1;
+	int outputFD = -1;
+
+	const char* inputPath = "STDIN";
+	if(argc < 2) {
+		inputFD = 0;
+	} else {
+		inputPath = argv[1];
+		inputFD = open(inputPath, O_RDONLY);
+		if(inputFD < 0) {
+			printf("Cannot open file %s\n", inputPath);
+			return 1;
+		}
+	}
+
+	if(argc < 3) {
+		outputFD = 1;
+	} else {
+		char* outputPath = argv[2];
+		outputFD = open(outputPath, O_WRONLY);
+		if(outputFD < 0) {
+			printf("Cannot open file %s\n", outputPath);
+			return 1;
+		}
+	}
+
+	struct stat fileStat;
+	if(fstat(inputFD, &fileStat) != 0) {
+		printf("Cannot stat file %s\n", inputPath);
+		return 1;
+	}
+
+	processFile(inputFD, outputFD, &fileStat);
+
+	if(outputFD == 1) {
+		printf("\n");
+	}
+}
+
+void processFile(int inputFD, int outputFD, struct stat* const fileStat) {
+	char* fullData = new char[fileStat->st_size];
+	int bytes = read(inputFD, fullData, fileStat->st_size);
+	fullData[fileStat->st_size] = 0;
+
+	if (bytes != fileStat->st_size) {
+		printf("Could not read entire file\n");
+	} else {
+		char* buffer = new char[4096];
+		int offset = 0;
+		readToken(fullData, offset, buffer, 4096);
+		lowercaseToken(buffer);
+		writeToken(buffer, outputFD);
+		while(readToken(fullData, offset, buffer, 4096)) {
+			writeToken(" ", outputFD);
+			lowercaseToken(buffer);
+			writeToken(buffer, outputFD);
+		}
+	}
+	delete fullData;
+}
+
